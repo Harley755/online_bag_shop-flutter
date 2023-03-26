53cc0c039ee926a873b2c11cb7d586241bd9a92a
Brice G <bricegoudalo@gmail.com>
2023-03-26 18:22:24 +0200

    add cart to details screen

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:online_bag_shop/constants.dart';
 import 'package:online_bag_shop/model/product.dart';
+import 'package:online_bag_shop/provider/product_provider.dart';
+import 'package:online_bag_shop/screens/cart/cart_screen.dart';
 import 'package:online_bag_shop/screens/details/components/body.dart';
+import 'package:provider/provider.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Product product;
@@ -10,14 +13,16 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final myCartLength = context.watch<ProductProvider>().getCart;
+
     return Scaffold(
       backgroundColor: product.color,
-      appBar: buildAppBar(context),
+      appBar: buildAppBar(context, myCartLength),
       body: Body(product: product),
     );
   }
 
-  AppBar buildAppBar(BuildContext context) {
+  AppBar buildAppBar(context, myCartLength) {
     return AppBar(
       backgroundColor: product.color,
       elevation: 0.0,
@@ -33,9 +38,27 @@ class DetailsScreen extends StatelessWidget {
           onPressed: () {},
           icon: SvgPicture.asset('assets/icons/search.svg'),
         ),
-        IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset('assets/icons/cart.svg'),
+        Stack(
+          alignment: Alignment.topRight,
+          children: [
+            IconButton(
+              onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const CartScreen(),
+              )),
+              icon: SvgPicture.asset("assets/icons/cart.svg"),
+              color: kTextColor,
+            ),
+            Container(
+              padding: EdgeInsets.zero,
+              height: 21.0,
+              width: 21.0,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.red,
+              ),
+              child: Center(child: Text(myCartLength.length.toString())),
+            )
+          ],
         ),
         const SizedBox(width: kDefaultPaddin / 2)
       ],
